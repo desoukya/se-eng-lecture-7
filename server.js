8e47b58678683507d54e305c610bc7eeb793b5bd
Amr Desouky <desoukya@gmail.com>
2021-12-20 10:47:00 +0200

    add swagger

--- server.js
+++ server.js
@@ -3,25 +3,19 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 
-// Parse request bodies in middleware before handlers
-const bodyParser  = require('body-parser');
-app.use(bodyParser.json());
-
 // Allow CORS
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,POST');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
 // Create HTTP Server and Listen for Requests
-app.listen(3000, async (req, res) => {
+app.listen(3000, async function(req, res) {
   console.log(`[OK] = HTTP Web Server Running On Port ${3000}`);
 
   // register API Endpoints
   app.get('/api/v1/students', async (req, res) => {
-    const knexResults = await db.select('*').from('students');
-    return res.json(knexResults)
+    const results = await db.select('*').from('students');
+    return res.json(results)
   });
 });
