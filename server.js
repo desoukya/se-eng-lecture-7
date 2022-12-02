f3639f82f73af3b919327c6f4004fbe2ba9dcec1
Amr Desouky <desoukya@gmail.com>
2022-12-02 15:14:18 -0600

    move endpoints to root

--- server.js
+++ server.js
@@ -4,30 +4,27 @@ const app = express();
 const db = require('./db');
 const { v4 } = require('uuid');
 
-// Handle POST requests with JSON payloads in request body
+// Configuration setup to parse JSON payloads from HTTP POST request body
 app.use(express.json());
 
+// Register HTTP endpoint to create new student
+app.post('/api/v1/students', async (req, res) => {
+  const newStudent = {
+    id: v4(),
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    major: req.body.major,
+  };
+  const studentRecord = await db('students').insert(newStudent).returning('*');
+  return res.json(studentRecord);
+});
+
+// Register HTTP endpoint to get all students
+app.get('/api/v1/students', async (req, res) => {
+  console.log('GET endpoint')
+  const results = await db.select('*').from('students');
+  return res.json(results)
+});
 
 // Create HTTP Server and Listen for Requests
-app.listen(3000, async function(req, res) {
-  console.log(`[OK] = HTTP Web Server Running On Port ${3000}`);
-
-  // Endpoint to create new student
-  app.post('/api/v1/students', async (req, res) => {
-    const newStudent = {
-      id: v4(),
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      major: req.body.major,
-    };
-    const studentRecord = await db('students').insert(newStudent).returning('*');
-    return res.json(studentRecord);
-  });
-
-  // Endpoint to get all students
-  app.get('/api/v1/students', async (req, res) => {
-    console.log('GET endpoint')
-    const results = await db.select('*').from('students');
-    return res.json(results)
-  });
-});
+app.listen(3000);
